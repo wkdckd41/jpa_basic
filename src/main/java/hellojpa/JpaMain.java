bef03efd385a7d89f18d12dfa0e539b8ef28fbc3
wkdckd41 <wkdckd41@gmail.com>
2024-02-12 00:00:06 +0900

    2-2 Hello JPA - 애플리케이션 개발

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,7 +24,6 @@ public class JpaMain {
             for (Member member : result) {
                 System.out.printf("member.name = " + member.getName());
             }
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
