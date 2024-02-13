e28aa2cd93e7040d862920a9f411341b85d17360
wkdckd41 <wkdckd41@gmail.com>
2024-02-13 23:53:21 +0900

    3-1 영속성 관리 - 내부 동작 방식 - 영속성 컨텍스트 2

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,9 +4,18 @@ import javax.persistence.Id;
 
 @Entity
 public class Member {
+
     @Id
     private Long id;
     private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
