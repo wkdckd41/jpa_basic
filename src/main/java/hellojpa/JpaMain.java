b2fc9bdeb6ce51308597bc8f1cbe57c0bb6832cf
wkdckd41 <wkdckd41@gmail.com>
2024-03-02 14:59:33 +0900

    8 프록시와 연관관계 관리 - 프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,11 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -17,19 +18,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); //프록시 객체의 클래스 이름을 출력
+            Hibernate.initialize(refMember); //프록시 객체를 강제 초기화
+
             tx.commit(); //트랜잭션 커밋
         } catch (Exception e) { //예외 처리
             tx.rollback(); //트랜잭션 롤백
+            e.printStackTrace();
         } finally {
             em.close(); //영속성 컨텍스트를 종료
         }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,8 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Member extends BaseEntity{
@@ -18,17 +16,6 @@ public class Member extends BaseEntity{
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) // 읽기 전용으로 설정
     private Team team;
 
-    @OneToOne // 일대일 관계
-    @JoinColumn(name = "LOCKER_ID") // 외래키가 있는 곳에 걸어준다.
-    private Locker locker;
-
-    @ManyToMany // 다대다 관계
-    @JoinTable(name = "MEMBER_PRODUCT") // 연결 테이블을 만들어준다.
-    private List<Product> products = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member") // 양방향 매핑
-    private List<MemberProduct> memberProducts = new ArrayList<>();
-
     public Long getId() {
         return id;
     }
@@ -43,6 +30,10 @@ public class Member extends BaseEntity{
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
     }
 
 }
